4c295088039d612ed08925e86c97e2647c681b70
Jitendra Singh Rathore <jitendrasingh.rathore@dotsquares.com>
2023-07-13 12:08:42 +0530

    add style

--- App.js
+++ App.js
@@ -1,8 +1,25 @@
 import React from "react";
-import  ReactDOM  from "react-dom";
-const element1 = React.createElement('h1', {id:"txtFname"}, 'Hello React');
-const element2 = React.createElement('h2', {id:"txtLname"}, 'Hello Javascript');
-const element3 = React.createElement('div',{id:"txtFullname"},[element1,element2]);
-//element3.appendchild([element1,element2])
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(element3);
+import ReactDom from "react-dom/client";
+
+
+
+const Fuctionalcomponent1 = ()=>{
+  return (<>
+  <p style={{color: "red"}}>this is javascript</p>
+  </>)
+}
+
+const Fuctionalcomponent2 = () => {
+  return <>
+    <p style={{color: "green"}}>this is react</p>
+  </>
+}
+const Fuctionalcomponent3 = () => {
+  return( 
+  <>
+  <Fuctionalcomponent1/>
+  <Fuctionalcomponent2/>
+  </>)
+}
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(<Fuctionalcomponent3 />);
